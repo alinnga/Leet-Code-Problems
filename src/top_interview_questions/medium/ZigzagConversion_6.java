04aef354b55e8c9b07c081a2d354afb1480ba1f4
alinnga <alina.parfenteva@mail.ru>
2023-04-13 13:10:59 +0300

    refactor

--- src/top_interview_questions/medium/ZigzagConversion_6.java
+++ src/top_interview_questions/medium/ZigzagConversion_6.java
@@ -8,44 +8,43 @@ public class ZigzagConversion_6 {
     }
 
     public static String convert(String s, int numRows) {
-        if(numRows == 1 || numRows >= s.length()){
+        if (numRows == 1 || numRows >= s.length()) {
             return s;
         }
         char[] array = s.toCharArray();
         int len = array.length;
-        int step = (numRows-1)*2;
+        int step = (numRows - 1) * 2;
         StringBuilder newStr = new StringBuilder();
-        for(int row = 0; row < numRows; row++){
+        for (int row = 0; row < numRows; row++) {
             int index = row;
             newStr.append(array[index]);
 
-            int step1 = step-row*2;
-            int step2 = row*2;
+            int step1 = step - row * 2;
+            int step2 = row * 2;
             int index1;
             int index2;
-            if(step1!=0 && step2 !=0){
-                index1 = index+step1;
-                index2 = index1+step2;
+            if (step1 != 0 && step2 != 0) {
+                index1 = index + step1;
+                index2 = index1 + step2;
 
-                while(true) {
-                    if(index1 >= len){
+                while (true) {
+                    if (index1 >= len) {
                         break;
                     }
                     newStr.append(array[index1]);
 
-                    if(index2 >= len){
+                    if (index2 >= len) {
                         break;
                     }
                     newStr.append(array[index2]);
-                    index1 = index2+step1;
-                    index2 = index1+step2;
+                    index1 = index2 + step1;
+                    index2 = index1 + step2;
                 }
-            }
-            else{
-                index = index+step;
-                while(index < len) {
+            } else {
+                index = index + step;
+                while (index < len) {
                     newStr.append(array[index]);
-                    index = index+step;
+                    index = index + step;
                 }
             }
         }
