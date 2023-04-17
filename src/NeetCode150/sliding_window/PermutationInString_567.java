c89140801d0faef25535a2af50b34a1a0ab52983
alinnga <alina.parfenteva@mail.ru>
2023-04-17 11:27:14 +0300

    reuse s1map

--- src/NeetCode150/sliding_window/PermutationInString_567.java
+++ src/NeetCode150/sliding_window/PermutationInString_567.java
@@ -11,20 +11,24 @@ public class PermutationInString_567 {
     public static boolean checkInclusion(String s1, String s2) {
         int winLen = s1.length();
         int start = 0;
+        int[] s1map = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1map[s1.charAt(i) - 'a']++;
+        }
+
         for(int end=start+winLen-1; end<s2.length(); end++, start++){
-            if(isPermutation(s1, s2.substring(start, end+1))){
+            if(isPermutation(s1map, s2.substring(start, end+1))){
                 return true;
             }
         }
         return false;
     }
-    public static boolean isPermutation(String s1,  String s2){
-        int[] s1map = new int[26];
+    public static boolean isPermutation(int[] s1map,  String s2){
         int[] s2map = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            s1map[s1.charAt(i) - 'a']++;
+        for (int i = 0; i < s2.length(); i++) {
             s2map[s2.charAt(i) - 'a']++;
         }
+
         for(int i =0; i< 26; i++){
             if(s1map[i]!=s2map[i]){
                 return false;
